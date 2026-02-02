340c4103b1da80898633d9bf4ad807485d216d2b
elsidpanolinocmg <elsidpanolino.cmg@gmail.com>
2026-02-02 14:21:44 +0800

    Add link to QSR UK ans AUS to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,13 +20,19 @@ export default function Home() {
     <div className="bg-transparent min-h-screen flex items-start sm:items-center justify-center flex-col gap-4 px-2 py-8">
       <h1 className="text-xl font-bold">CMG GA4 Dashboard</h1>
       <div className="flex flex-col gap-2">
-        
+
         <Link href="/dashboard/editorial" className="hover:underline">
           Editorial Dashboard
         </Link>
+        <Link href="/dashboard/editorial/qsr-uk" className="hover:underline ml-4">
+          QSR UK Editorial Dashboard
+        </Link>
+        <Link href="/dashboard/editorial/qsr-aus" className="hover:underline ml-4">
+          QSR AUS Editorial Dashboard
+        </Link>
 
 
-        <Link href="/all-active?grouped=true" className="hover:underline">
+        <Link href="/all-active?grouped=true" className="hover:underline mt-4">
           View active users
         </Link>
 
